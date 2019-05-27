e896e59f1f07266dd360b5b090a8f32de3cc1958
Juan Carlo Banayo <johncban@gmail.com>
2019-05-27 22:05:21 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -7,12 +7,11 @@ module Memorable
     def count
       self.all.count
     end
+  end
 
-    module InstanceMethods
-      def initialize
+  module InstanceMethods
+    def initialize
 
-      end
     end
-
   end
 end
