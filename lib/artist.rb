d10cb851ad8831040ae112b7f341ca80c7242d13
Juan Carlo Banayo <johncban@gmail.com>
2019-05-27 21:18:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,21 +22,20 @@ class Artist
   def self.all
     @@artists
   end
+
+  def self.find_by_name(name)
+    @@artists.detect {|a| a.name == name}
+  end
+
 =begin
   def self.reset_all
     self.all.clear
   end
 =end
+
   def self.count
     self.all.count
   end
-
-  def self.find_by_name(name)
-    @@artists.detect {
-      |a| a.name == name
-    }
-  end
-  
 
   def add_song(song)
     @songs << song
